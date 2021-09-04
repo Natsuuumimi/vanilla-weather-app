b18a09835fcb6b00eb5b2e747624d390b73e9e49
Natsuuumimi <natsuuumimi@gmail.com>
2021-09-04 18:04:16 +0900

    changed city default & API integration to Forecast

--- src/app.js
+++ src/app.js
@@ -18,28 +18,53 @@ function formatDate(timestamp) {
 	return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+ let date = new Date(timestamp * 1000);
+ let day = date.getDay();
+ let days = ["Sun", "Mon","Tue","Wed","Thu","Fri","Sat"];
+
+ return days[day];
+}
+
 function displayForecast(response) {
+	let forecast = response.data.daily;
+
 	let forecastElement = document.querySelector("#forecast");
 
 	let forecastHTML = `<div class="row">`;
-  let days = ["Mon","Tue","Wed","Thu","Fri"];
-  days.forEach(function(day) {
-	forecastHTML =
-		forecastHTML +
-		`
+	forecast.forEach(function (forecastDay, index) {
+		if (index < 5) {
+			forecastHTML =
+				forecastHTML +
+				`
       <div class="col">
-        <div class="weather-forecast-date">${day}</div>
-        <img src="https://ssl.gstatic.com/onebox/weather/64/rain.png" alt="" width="30px">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${
+					forecastDay.weather[0].icon
+				}@2x.png" alt="" width="44px">
          <div class="temperature2">
-            <span class="forecast-max">18°</span>
-            <span class="forecast-min">22°</span>
+            <span class="forecast-max">${Math.round(
+							forecastDay.temp.max
+						)}°</span>
+            <span class="forecast-min">${Math.round(
+							forecastDay.temp.min
+						)}°</span>
          </div>
       </div>
   `;
-  })
+		}
+	});
 
 	forecastHTML = forecastHTML + `</div>`;
 	forecastElement.innerHTML = forecastHTML;
+}
+
+//5days forecast display
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "023ad625bfdcd400727da5f0c2fed554";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 //current city&temp
@@ -66,18 +91,21 @@ function displayWeather(response) {
 		`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 	);
 	iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
+
 //display the city & locationTemp
-function searchCity(city) {
+function search(city) {
 	let apiKey = "023ad625bfdcd400727da5f0c2fed554";
 	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 	axios.get(apiUrl).then(displayWeather);
 }
 
-function search(event) {
+function handleSubmit(event) {
 	event.preventDefault();
-	let city = document.querySelector("#city-input").value;
-	searchCity(city);
+	let cityInputElemet = document.querySelector("#city-input").value;
+	search(cityInputElemet.value);
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
@@ -113,12 +141,6 @@ function displayCelsiusTemperature(event) {
 	let temp = document.querySelector("#temp");
 	temp.innerHTML = Math.round(celsiusTemperature);
 }
-//5days forecast display
-function getForecast(coordinates){
-  let apiKey = "023ad625bfdcd400727da5f0c2fed554";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
 
 let button = document.querySelector("#b1");
 button.addEventListener("click", getCurrentPosition);
@@ -131,6 +153,5 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let celsiusTemperature = null;
 
-displayForecast();
 
-getForecast(response.data.coord);
+search("Tokyo");
